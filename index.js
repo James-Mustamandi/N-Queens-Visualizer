6ab1699602701fd714eeb388e6cc93a237d77423
James Mustamandi <jmustama@Mustfordmforn>
2024-10-16 21:56:54 -0700

    Improve input page

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const maxValueN = 12;
+
 function initResult() {
     n = document.getElementById('n').value; 
     localStorage.setItem('n', n);
 }
+
+
+
+document.querySelectorAll('.odd-tile').forEach(button => {
+    button.addEventListener('click', function() {
+        const n = this.getAttribute('data-value');
+        localStorage.setItem('n', n);
+        window.location.href = 'result.html';
+    });
+});
+
+document.querySelectorAll('.even-tile').forEach(button => {
+    button.addEventListener('click', function() {
+        const n = this.getAttribute('data-value');
+        localStorage.setItem('n', n);
+        window.location.href = 'result.html';
+    });
+});
 
